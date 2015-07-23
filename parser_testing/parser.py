ea2d5c2635297170813c18b984df0f29dd9aeb1d
Alex Dent <aldent95@hotmail.com>
2015-07-24 11:02:24 +1200

    Added command line functionality
    Added functionalityto the parser to receive the file from the command
    line.
    Also changed the gitignore to ignore some useless output files.

--- parser_testing/parser.py
+++ parser_testing/parser.py
@@ -1,11 +1,22 @@
 import xml.etree.ElementTree as ET
+import argparse
 import os
 import sys
 
 
-os.chdir(os.path.dirname(__file__)+"\data")
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+argparse = argparse.ArgumentParser(description="Load XML file into program")
+argparse.add_argument('-i', dest='filename',required=True,help='Input xml log file',metavar='FILE', type=lambda x: is_valid_file(argparse,x))
+args=argparse.parse_args()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 ET.register_namespace("","http://www.xes-standard.org/")
-tree = ET.parse('BPIC15_1.xes.xml')
+tree = ET.parse(args.filename)
 print ('Done')
 root = tree.getroot()
 print root.tag
