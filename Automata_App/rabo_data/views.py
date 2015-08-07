08ef5483f611ec1ac788969a654b6685b8d69a23
Alex Dent <aldent95@hotmail.com>
2015-08-07 15:20:56 +1200

    STILL MORE NAME FIXES

--- Automata_App/rabo_data/views.py
+++ Automata_App/rabo_data/views.py
@@ -1,20 +1,20 @@
-from rabo_data.models import Interaction_Activity
-from rabo_data.serializers import Interaction_ActivitySerializer
+from rabo_data.models import Incident_Activity
+from rabo_data.serializers import Incident_ActivitySerializer
 from rest_framework import generics
 from django.http import HttpResponse
 
 
 class Incident_ActivityList(generics.ListCreateAPIView):
-    queryset = Interaction_Activity.objects.all()
-    serializer_class = Interaction_ActivitySerializer
+    queryset = Incident_Activity.objects.all()
+    serializer_class = Incident_ActivitySerializer
 
 
 class Incident_ActivityDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Interaction_Activity.objects.all()
-    serializer_class = Interaction_ActivitySerializer
+    queryset = Incident_Activity.objects.all()
+    serializer_class = Incident_ActivitySerializer
 
 
 def total(request):
-    queryset = Interaction_Activity.objects.all()
+    queryset = Incident_Activity.objects.all()
     data = {"total actitivities": len(queryset)}
     return HttpResponse(str(data))
