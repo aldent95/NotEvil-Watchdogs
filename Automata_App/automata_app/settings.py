8e9a72a56dfdb57d9dc6b75eea4eb12558710ac2
Daniel Cole <coledani2@stellar.ecs.vuw.ac.nz>
2015-08-14 14:54:16 +1200

    style

--- Automata_App/automata_app/settings.py
+++ Automata_App/automata_app/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__), 'static/')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
