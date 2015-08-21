e4fe22f339a35dfc2110cde545a01424d92aaba3
Daniel Cole <coledani2@stellar.ecs.vuw.ac.nz>
2015-08-21 14:24:52 +1200

    merged visualisation onto server

---
+++ Automata_App/automata_app/static/get_input.js
@@ -0,0 +1,43 @@
+var data;
+var inputNodes = [];
+var inputLinks = [];
+var inputCount = count;
+var minCount = 300;
+
+function parseData(parentIndex, aNode, root){
+	inputCount++
+	count ++ ;
+	var current = inputCount - 1;
+	addNode(current, 1, aNode.event, inputCount, inputCount, root);
+
+	if(parentIndex != null){
+		addLink(parentIndex, current, 1);
+	}
+
+	if(aNode.links != null){
+		jQuery.each(aNode.links, function(index, value){
+			if(value.count > minCount){
+				parseData(current, value.child, false);
+			}
+		})
+	}
+
+}
+
+/*
+$.getJSON("trie.json", function(json) {
+    parseData(null, json, true);
+    console.log(inputCount);
+});
+
+*/
+
+//gets data from endpoint specified on server. 
+function getData(handleData) {
+  $.ajax({
+    url:"http://150.242.41.175:5050/rabo/Interaction_Activitys/trie2"
+    success:function(data) {
+      parseData(null, JSON.parse(data), true); 
+    }
+  });
+}
