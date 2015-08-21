6f6990b22a826792efb09acea99929aa9867e3d2
Daniel Cole <coledani2@stellar.ecs.vuw.ac.nz>
2015-08-21 14:39:37 +1200

    added endpoint visualisation

--- Automata_App/web_views/views.py
+++ Automata_App/web_views/views.py
@@ -5,3 +5,8 @@ def index(request):
 	context = {}
 	template = loader.get_template("index.html")
 	return HttpResponse(template.render(context))
+
+def visualisation1(request):
+	context = {}
+	template = loader.get_template("visualisation1.html")
+	return HttpResponse(template.render(context))
