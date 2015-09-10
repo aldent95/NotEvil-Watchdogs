92a56c0a71063a390fd1a962408c61eb348b0dbd
Alex Dent <aldent95@hotmail.com>
2015-09-11 11:01:51 +1200

    invalid syntax fix for views.py

--- Automata_App/web_views/views.py
+++ Automata_App/web_views/views.py
@@ -5,7 +5,7 @@ from forms import AppRegistrationForm
 def index(request):
     return HttpResponse("Hello, world. You're at the web_views index.")
 def signup(request):
-    if request.method = 'POST':
+    if request.method == 'POST':
         form = AppRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
