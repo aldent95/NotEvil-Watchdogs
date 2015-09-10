2b3d0ff2c4e703bcbbeaf607ad77458f73bf9339
Alex Dent <aldent95@hotmail.com>
2015-09-11 10:05:10 +1200

    Added missing change from signup addition

--- Automata_App/web_views/views.py
+++ Automata_App/web_views/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from forms import AppRegistrationForm
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the web_views index.")
+def signup(request):
+    if request.method = 'POST':
+        form = AppRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('registration/signup_complete.html')
+        args={}
+        args.update(csrf(request))
+        args.['form'] = AppRegistrationForm()
+        print args
+        return render(request, 'registration/signup.html', args)
