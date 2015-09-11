22eacdb741c01a7f57985303ec884edfd7027fcd
Thiodexal <mahoneyd08@gmail.com>
2015-09-11 12:41:50 +1200

    More label toggling

--- Automata_App/automata_app/static/buttons.js
+++ Automata_App/automata_app/static/buttons.js
@@ -1,9 +1,10 @@
-
+var labelsShown = true;
 var body = d3.select("body");
 var buttonPanel = body.append("div");
 var labelButton = body.append("button");
 
 labelButton.on("click", function(d){
 	alert("Boo");
-	body.selectAll(".node text").attr("visibility", "hidden");
+	if(labelsShown){body.selectAll(".node text").attr("visibility", "hidden");}
+	else{body.selectAll(".node text").attr("visibility", "inline");}
 })
