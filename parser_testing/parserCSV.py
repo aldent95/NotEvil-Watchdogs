67460358832ad3930e570292ca7f1ad18cc3618f
adrian-turjak <taydus.a.drax@gmail.com>
2015-09-11 12:01:05 +1200

    MERGE sort of, of old parser branch

--- parser_testing/parserCSV.py
+++ parser_testing/parserCSV.py
@@ -4,50 +4,60 @@ import sys
 import json
 import csv
 import requests
+import string
 from requests.exceptions import ConnectionError
 from datetime import datetime
-
-fmt = '%d-%m-%Y %H:%M:%S'
-
+import re
+import databasePopulator as dbpop
+from rabo_data.models import Incident_Activity
 def is_valid_file(parser, arg):
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
         return arg
-
-argparse = argparse.ArgumentParser(description="Load XML file into program")
-argparse.add_argument('-i', dest='filename',required=True,help='Input xml log file',metavar='FILE', type=lambda x: is_valid_file(argparse,x))
-args=argparse.parse_args()
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-fieldnames = []
-rows = []
-i = 0
-with open(args.filename,'r') as infile:
-    for line in infile:
+def parseFile(fileObject, filename):
+    datere = re.compile('^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$')
+    fmt1 = '%d-%m-%Y %H:%M'
+    fmt2 = '%d-%m-%Y %H:%M:%S'
+    fieldnames = []
+    rows = []
+    i = 0
+    print fileObject
+    data = fileObject.read()
+    data = data.split('\n')
+    print filename
+    print 'reading'    
+    for line in data:
+	print line
+        line.strip('\r')
         if i == 0:
+            
             row = line.split(';')
+	    print row
+            return
             for field in row:
+                if field.startswith('#'):
+                    field.strip('#')
+                    field = "Number " + field
                 fieldnames.append(field.strip("(aff)").replace(' ', '_').strip('\n'))
             i = i+1
         else:
             rows.append(line)
-fieldnames = tuple(fieldnames)
-csvfile = csv.DictReader(rows, fieldnames, delimiter=';')
-print ('Done')
-
-url = 'http://150.242.41.175:5050/rabo/Interaction_Activity/'
-headers = {'Content-Type': 'application/json'}
-for row in csvfile:
-    date = datetime.strptime(row['DateStamp'], fmt)
-    row['DateStamp'] = str(date)
-    try:
-        response = requests.post(url, headers=headers, data=json.dumps(row))
-        if response.status_code != 201:
-            print response.json()
-    except ConnectionError as e:
-        print e
-
+    
+    fieldnames = tuple(fieldnames)
+    csvfile = csv.DictReader(rows, fieldnames, delimiter=';')
+    print ('Done')
+    for row in csvfile:
+        for key in row:
+            if datere.match(row[key].split(' ')[0]):
+                date = 0
+                try:
+                    date = datetime.strptime(row[key], fmt1)
+                except ValueError:
+                    date= datetime.strptime(row[key], fmt2)
+                row[key] = str(date)
+        dbpop.populate(row, filename)
+    print 'fully finished loading'
 
 
 
