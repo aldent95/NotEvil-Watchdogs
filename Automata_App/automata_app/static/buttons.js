6ad6b227d3c790a9f6477d58b4934b342ab0cb5d
Thiodexal <mahoneyd08@gmail.com>
2015-09-11 13:30:10 +1200

    M

--- Automata_App/automata_app/static/buttons.js
+++ Automata_App/automata_app/static/buttons.js
@@ -6,16 +6,16 @@ var labelsShown = true;
 var body = d3.select("body");
 var buttonPanel = body.append("div")
 	.attr("height", 200)
-	.attr("width", buttonWidth)
+	.attr("min-width", buttonWidth)
 	.attr("class", "button_panel");
 
-var labelButton = buttonPanel.append("svg")
+var labelButton = buttonPanel.append("button")
 	.attr("style")
 	.html("text")
 	.attr("height", 200)
-	.attr("width", 400);
-
-labelButton.on("click", function(d){
+	.attr("width", 400)
+	.attr("min-height", buttonHeight)
+	.on("click", function(d){
 	if(labelsShown){body.selectAll(".node text").attr("visibility", "hidden");labelsShown = false;}
 	else{body.selectAll(".node text").attr("visibility", "inline");labelsShown = true;}
 })
