9c798fe1cf5a50847ea8a8480adccc5e4cd4e3af
Thiodexal <mahoneyd08@gmail.com>
2015-09-11 12:47:46 +1200

    Removed spooky ghost

--- Automata_App/automata_app/static/buttons.js
+++ Automata_App/automata_app/static/buttons.js
@@ -1,10 +1,12 @@
+var buttonHeight = 50;
+
 var labelsShown = true;
+
 var body = d3.select("body");
-var buttonPanel = body.append("div");
+var buttonPanel = body.append("div").attr("height", buttonHeight);
 var labelButton = body.append("button");
 
 labelButton.on("click", function(d){
-	alert("Boo");
 	if(labelsShown){body.selectAll(".node text").attr("visibility", "hidden");labelsShown = false;}
 	else{body.selectAll(".node text").attr("visibility", "inline");labelsShown = true;}
 })
