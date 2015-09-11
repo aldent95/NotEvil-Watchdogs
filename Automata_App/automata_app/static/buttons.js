589a18f26744a5f56c04512ff52da71298026d4c
Thiodexal <mahoneyd08@gmail.com>
2015-09-11 13:41:34 +1200

    P

--- Automata_App/automata_app/static/buttons.js
+++ Automata_App/automata_app/static/buttons.js
@@ -4,14 +4,11 @@ var buttonWidth = (window.innerWidth * 0.75) - 15
 var labelsShown = true;
 
 var body = d3.select("body");
-var buttonPanel = body.append("div")
-	.attr("height", 200)
-	.attr("min-width", buttonWidth)
-	.attr("class", "button_panel");
+var buttonPanel = body.append("div");
 
 var labelButton = buttonPanel.append("button")
-	.style("height", 200)
-	.style("width", 400)
+	.style("height", 100)
+	.style("width", 100)
 	.on("click", function(d){
 	if(labelsShown){body.selectAll(".node text").attr("visibility", "hidden");labelsShown = false;}
 	else{body.selectAll(".node text").attr("visibility", "inline");labelsShown = true;}
