6117caf1dc1b17562adc97613a4a7b8262981af1
copelajohn <chansey.killer@hotmail.com>
2015-09-25 11:08:13 +1200

    Added a Data structure for the parsed file

--- parser_testing/parserCSV.py
+++ parser_testing/parserCSV.py
@@ -5,18 +5,29 @@ import json
 import csv
 import requests
 import string
-from requests.exceptions import ConnectionError
-from datetime import datetime
 import re
 import databasePopulator as dbpop
+import pprint
+
+from requests.exceptions import ConnectionError
+from datetime import datetime
 from rabo_data.models import Incident_Activity
+
+
+#checks the validity of File
 def is_valid_file(parser, arg):
+    # Returns true Boolean value if file path is wrong or invalid file
+    #  sends user error message else returns the file
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
         return arg
+
 def parseFile(fileObject, filename):
+    # 
     datere = re.compile('^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$')
+    #Date Time Regular Expression format 1, Day:Month:Year ; Hours:Minutes, 
+    #format 2, Day:Month:Year ; Hours:Minutes:Seconds
     fmt1 = '%d-%m-%Y %H:%M'
     fmt2 = '%d-%m-%Y %H:%M:%S'
     fieldnames = []
@@ -59,6 +70,38 @@ def parseFile(fileObject, filename):
         dbpop.populate(row, filename)
     print 'fully finished loading'
 
+    """ 
+    This reads in each row of the parsed data and groups them by
+    the Incident ID
+    """
+    groupedData = {}
+    for row in csvfile:
+        try: 
+            #probably need to amend something in here but i can't think today my brain is broken
+            groupedData[row["Incident ID"]].append(row)
 
+
+        except KeyError: 
+            groupedData[row["Incident ID"]] = [row,]
+
+
+    for key, rows in groupedData.iteritems():
+
+        metaData = {"Incident ID": key} #leaving Empty right now, for later use
+        events = [] #Events metadata for sorting
+
+        for row in rows:
+            eventMetaData{"DateStamp": row["DateStamp"], "ActNum": row["IncidentActivity_Number"], 
+                "ActType": row["IncidentActivity_Type"], "AssignGroup": row["Assignment Group"], "KMNum": row["KM number"], "InteractionID": row["Interaction ID"]}                       
+            eventObj = {"Name" : eventMetaData["ActType"], "DateStamp" : eventMetaData["DateStamp"], "metadata" : eventMetaData } #contains each event sorted for dict
+            events.append(eventObj)
+
+
+        data = {"Events" : events, "MetaData" : metaData}
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(data)
+
+
+ 
 
 
