49eefb73377aab7c9c0283109cb7747a0895b9cc
Daniel Cole <coledani2@stellar.ecs.vuw.ac.nz>
2015-09-25 15:12:54 +1200

    pls work

--- Automata_App/automata_app/static/filter.js
+++ Automata_App/automata_app/static/filter.js
@@ -9,14 +9,20 @@ var dateSlider = d3.slider().scale(d3.scale.ordinal().domain([
 d3.select('#dateSlider').call(dateSlider);
 */
 var freqSlider = d3.slider().value(0).on("slide", function(evt, value) {
-    d3.select('#freqText').text(Math.round(value*3));
+    d3.selectAll('#freqText').text(Math.round(value*3));
 });
 
 
     
-//d3.select('#freqSlider').call(freqSlider);
+d3.select('#freqSlider').call(freqSlider);
 
-d3.select(".buttonPanel").append("svg").style("height", buttonHeight).style("width", 300).call(freqSlider);
+
+d3.select(".buttonPanel").append("div").html("<br>"); //need a space, haven't aligned yet
+
+d3.select(".buttonPanel").append("div").style("height", buttonHeight).style("width", 300).call(freqSlider);
+var sliderPanel = d3.select(".buttonPanel").append("div").style("height", buttonHeight).style("width", 300).;
+	sliderPanel.append("span").attr("id", "freqText");
+	sliderPanel.append("div").call(freqSlider);
 buttonPanel.append("button").style("height", buttonHeight).style("width", buttonWidth).on("click", function(d){getValues()});
 function getValues(){
 
