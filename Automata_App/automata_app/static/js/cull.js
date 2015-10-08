19f3b7184e036a804e8a86d1b48dd10efb184f99
Thiodexal <mahoneyd08@gmail.com>
2015-10-09 12:45:42 +1300

    Culling for ratio should work

--- Automata_App/automata_app/static/js/cull.js
+++ Automata_App/automata_app/static/js/cull.js
@@ -5,11 +5,17 @@ var first = true;
 
 
 function cullFromRatio(parentNode, parentSize){
-	if(first){
-		var children = 0;
-		for (e in parentNode.links) { element_count++; }
-		console.log(parentNode.links);
-		console.log(children);
-		first = false;
+	var children = 0;
+	for (e in parentNode.links) { children++; }
+		if(children > 1){
+		expectedPro = 1 / children;
+		minPro = expectedPro * acceptedRatio * parentSize;
+		jQuery.each(aNode.links, function(index, value){
+			if(value.count >= minPro){
+				parseData(null, value.child, value.count, true);
+			}
+		});
 	}
+	else if(children == 1){parseData(null, value.child, value.count, true);}
+	else{console.log("Cull called without children: ");}
 }
