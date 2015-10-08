3865a5d89ae4e287cc31be85459eb025d70225d0
Thiodexal <mahoneyd08@gmail.com>
2015-10-09 12:31:46 +1300

    testing a child counter

--- Automata_App/automata_app/static/js/cull.js
+++ Automata_App/automata_app/static/js/cull.js
@@ -5,5 +5,11 @@ var first = true;
 
 
 function cullFromRatio(parentNode, parentSize){
-	if(first){console.log(parentNode.links);first = false;}
+	if(first){
+		var children = 0;
+		for (e in parentNode.links) { element_count++; }
+		console.log(parentNode.links);
+		console.log(children);
+		first = false;
+	}
 }
