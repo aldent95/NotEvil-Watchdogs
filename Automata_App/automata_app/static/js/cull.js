0801ee9409bd6ec24bf6df520cc9e7b3dc475e1b
Thiodexal <mahoneyd08@gmail.com>
2015-10-09 01:58:24 +0000

    Added ratio Culling

--- Automata_App/automata_app/static/js/cull.js
+++ Automata_App/automata_app/static/js/cull.js
@@ -1,25 +1,33 @@
-var expectedPro;
-var minPro;
-var acceptedRatio = 1;
+var culled = 0;
+var acceptedRatio = 1.2;
 var first = true;
+var smallest;
 
 
-function cullFromRatio(parentNode, parentSize){
+function cullFromRatio(parentNode, parentSize, curIndex){
 	var children = 0;
+	var minPro;
 	for (e in parentNode.links) { children++; }
 	
 	if(children > 1){
-		expectedPro = 1 / children;
-		minPro = expectedPro * acceptedRatio * parentSize;
+		minPro = acceptedRatio * parentSize / children;
+		//console.log(curIndex + " minPro " + minPro);
+		//console.log(curIndex + " parentSize " + parentSize);
+		//console.log(curIndex + " children " + children);
 		jQuery.each(parentNode.links, function(index, value){
-			if(value.count >= minPro){
-				parseData(current, value.child, value.count, false);
+			if(value.count > minPro){
+				if(value.count < smallest || smallest == null){smallest = value.count};
+				//console.log(curIndex + " " + index + " child " + value.count);
+				parseData(curIndex, value.child, value.count, false);
 			}
+			else{culled++;}
 		});
 	}
 	else if(children == 1){
 		jQuery.each(parentNode.links, function(index, value){
-			parseData(current, value.child, value.count, false);
+			if(value.count < parentSize / 2){
+				parseData(curIndex, value.child, value.count, false);
+			}
                 });
 	}
 	else{console.log("Cull called without children: ");}
--- Automata_App/automata_app/static/js/get_input.js
+++ Automata_App/automata_app/static/js/get_input.js
@@ -4,11 +4,11 @@ var inputLinks = [];
 function parseData(parentIndex, aNode, nodeSize, root){
 	count ++ ;
 	var current = count - 1;
-	if(count > 2500){return;}
+	if(count > 2500){console.log("Culled " + culled + " Nodes");return;}
 	if(isArrayEmpty(aNode.links)){addNode(current, calcNodeSize(nodeSize), aNode.event, count, count, root, false);}
 	else{
 		addNode(current, calcNodeSize(nodeSize), aNode.event, count, count, root, true);
-		cullFromRatio(aNode, nodeSize);
+		cullFromRatio(aNode, nodeSize, current);
 	}
 	if(parentIndex != null){
 		addLink(parentIndex, current, Math.sqrt(nodeSize) / Math.sqrt(minCount));
@@ -39,9 +39,9 @@ function parseRoot(aNode){
 }
 
 function calcNodeSize(nodeSize, type){
-	if(type === "log"){return r * Math.log(nodeSize) / Math.log(minCount);}				//log
-	if(type === "sqrt"){return r * Math.sqrt(nodeSize) / Math.sqrt(minCount);}			//sqrt
-	if(type === "cube"){return r * Math.pow(nodeSize, 1/3) / Math.pow(minCount, 1/3)}	//cbrt
+	if(type === "log"){return r * Math.log(nodeSize) / Math.log(smallest);}				//log
+	if(type === "sqrt"){return r * Math.sqrt(nodeSize) / Math.sqrt(smallest);}			//sqrt
+	if(type === "cube"){return r * Math.pow(nodeSize, 1/3) / Math.pow(smallest, 1/3)}	//cbrt
 	return nodeSize;
 }
 
@@ -50,11 +50,13 @@ if(test){
 	    storedTrie = json;
 	    parseRoot(storedTrie);
 	    console.log(count + " Nodes");
+	    console.log(culled + " Culled");
 	});
 } else{
 	$.getJSON("/automata/projects/"+ p_uuid +"/trie", function(json) {
 		storedTrie = json;
 	    parseRoot(storedTrie);
 	    console.log(count + " Nodes");
+	    console.log(culled + " Culled");
 	});
 };
