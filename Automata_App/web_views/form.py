59b012d89c4f752d4d9063d44be7d002ffd346f8
Alex Dent <aldent95@hotmail.com>
2015-10-16 15:11:48 +1300

    Commented ALL the things

--- Automata_App/web_views/form.py
+++ Automata_App/web_views/form.py
@@ -2,20 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class AppRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class AppRegistrationForm(UserCreationForm):#User signup form
+    email = forms.EmailField(required=True)#Set up all the fields 
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     username = forms.CharField(required=True)
     company_name = forms.CharField(required=False)
 
-    class Meta:
+    class Meta:#Meta user
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-    def save(self,commit = True):   
-        user = super(AppRegistrationForm, self).save(commit = False)
-        user.email = self.cleaned_data['email']
+    def save(self,commit = True):  #Saving the user 
+        user = super(AppRegistrationForm, self).save(commit = False)#Save the actual django user object and get it back
+        user.email = self.cleaned_data['email']#Then add all the extra fields
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.username = self.cleaned_data['username']
--- Automata_App/web_views/views.py
+++ Automata_App/web_views/views.py
@@ -11,58 +11,57 @@ from projects.models import Project
 from django.contrib.auth import logout as dj_logout
 
 
-def index(request):
+def index(request):#Basic Index View
     context = {}
     template = loader.get_template("index.html")
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))#Return the template as a rendered html webpage
 
 @login_required(login_url='/login/')
-@requires_csrf_token
-def visualisation(request, p_uuid):
+@requires_csrf_token#Makes sure this page gets the token needed to post to the API
+def visualisation(request, p_uuid):#Visualisation view
     try:
-        project = Project.objects.get(uuid=p_uuid)
+        project = Project.objects.get(uuid=p_uuid)#Try catch for getting a project, throws 404 if there is no project.
     except Project.DoesNotExist:
         return render(request, '404.html', {'errorMessage':'The project with the id '+p_uuid+' does not exist'})
-    context = {
+    context = {#Context variables to pass to the template
         "p_uuid": p_uuid,
         'p_name': project.name,
         'p_description': project.description,
         "test": False
     }
     template = loader.get_template("visualisation.html")
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))#Return the template as a rendered html webpage
 
-def test_visualisation(request):
-    context = {"test": True}
+def test_visualisation(request):#Test visualisation
+    context = {"test": True}#Context variables to pass to the template
     template = loader.get_template("visualisation.html")
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))#Return the template as a rendered html webpage
 
-def signup(request):
-    if request.method == 'POST':
-        form = AppRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/login')
-    else:
+def signup(request):#Signup page
+    if request.method == 'POST':#If we a receiving a a post request
+        form = AppRegistrationForm(request.POST)#Get the form that the user should have filled out
+        if form.is_valid():#and the form is valid
+            form.save()#If it is valid save it
+            return HttpResponseRedirect('/login')#And redirect to login page
+    else:#Otherwise
         args={}
-        args.update(csrf(request))
-        args['form'] = AppRegistrationForm()
-        print args
-        return render(request, 'registration/signup.html', args)
-    return render(request, 'registration/signup.html', {'form': form})
+        args.update(csrf(request))#Get the token for posting
+        args['form'] = AppRegistrationForm()#Get the form
+        return render(request, 'registration/signup.html', args)#Return the template as a rendered html webpage
+    return render(request, 'registration/signup.html', {'form': form})#Return the template as a rendered html webpage
 
 @login_required(login_url='/login/')
-@requires_csrf_token
-def projectshome(request):
-        context={}
+@requires_csrf_token#Makes sure this page gets the token needed to post to the API
+def projectshome(request):#Home page
+        context={}#Context variables to pass to the template
         user = request.user
         context['project_list'] = user.project_set.all()
         context['username'] = str(user)
-        return render(request, 'projects/home.html', context)
+        return render(request, 'projects/home.html', context)#Return the template as a rendered html webpage
         
 def logout(request):
         dj_logout(request)
-        context={}
+        context={}#Context variables to pass to the template
         template = loader.get_template('registration/logout.html')
-        return HttpResponse(template.render(context))
+        return HttpResponse(template.render(context))#Return the template as a rendered html webpage
         
